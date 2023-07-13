3549a45431eddd1b835e4930bcc0dd5ca676d871
gennadiy-gaysha <gennadiy-gaysha@gmail.com>
2023-07-13 23:13:21 +0200

    Implement Add Task fuctionality

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,4 +2,15 @@ document.addEventListener("DOMContentLoaded", function () {
   //sidenav initialization
   let sidenav = document.querySelectorAll(".sidenav");
   M.Sidenav.init(sidenav);
+
+  //   datepicker initialization
+  let datepicker = document.querySelectorAll(".datepicker");
+  M.Datepicker.init(datepicker, {
+    format: "dd mmm, yyyy",
+    i18n: { done: "Select" },
+  });
+
+  //   select initialization
+  var selects = document.querySelectorAll("select");
+  M.FormSelect.init(selects);
 });
